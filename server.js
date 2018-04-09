be42eff472a2052860614e59ccd68c5c2fcd3ca5
Eric <eric@wsib-innovation.ca>
2018-04-09 10:43:30 -0400

    trying to add graphql to node js server

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-const http = require("http");
-const app = require("./app");
-const port = process.env.PORT || 3002;
-const server = http.createServer(app);
-server.listen(port);
+var express = require("express");
+var express_graphql = require("express-graphql");
+var { buildSchema } = require("graphql");
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+var root = {
+  message: () => "Hello World!"
+};
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000, () =>
+  console.log("Express GraphQL Server Now Running On localhost:4000/graphql")
+);
