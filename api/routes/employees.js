b51d41eada5d0795201ec79305b596d374d00a5b
Eric <eric@wsib-innovation.ca>
2018-07-11 14:12:12 -0400

    updated nodearray route

--- api/routes/employees.js
+++ api/routes/employees.js
@@ -6,7 +6,7 @@ router.get("/nodeArray/:userId", (req, res, next) => {
   let appData = {};
   const id = req.params.userId;
   database.query(
-    "SELECT * FROM NodeArray WHERE userId = ? ORDER BY id",
+    "SELECT * FROM NodeArray WHERE employeeId = ? ORDER BY id",
     [id],
     function(err, rows, fields) {
       if (err) {
@@ -79,42 +79,42 @@ router.get("/:userId", function(req, res) {
   let appData = {};
   const id = req.params.userId;
   database.query(
-      "SELECT * FROM Notification WHERE userId = ? ORDER BY createdAt DESC",
-      [id],
-      function(err, rows, fields) {
-        if (err) {
-          appData.error = 1;
-          appData["data"] = "Error Occured!";
-          console.log(err);
-          res.status(400).json(appData);
-        } else {
-          appData["error"] = 0;
-          appData["data"] = rows;
-          res.status(200).json(appData);
-        }
+    "SELECT * FROM Notification WHERE userId = ? ORDER BY createdAt DESC",
+    [id],
+    function(err, rows, fields) {
+      if (err) {
+        appData.error = 1;
+        appData["data"] = "Error Occured!";
+        console.log(err);
+        res.status(400).json(appData);
+      } else {
+        appData["error"] = 0;
+        appData["data"] = rows;
+        res.status(200).json(appData);
       }
-    );
+    }
+  );
 });
 
 router.get("/getForms/:userId", function(req, res) {
   let appData = {};
   const id = req.params.userId;
-  database.query(
-      "SELECT * FROM Forms WHERE userId = ?",
-      [id],
-      function(err, rows, fields) {
-        if (err) {
-          appData.error = 1;
-          appData["data"] = "Error Occured!";
-          console.log(err);
-          res.status(400).json(appData);
-        } else {
-          appData["error"] = 0;
-          appData["data"] = rows;
-          res.status(200).json(appData);
-        }
-      }
-    );
+  database.query("SELECT * FROM Forms WHERE userId = ?", [id], function(
+    err,
+    rows,
+    fields
+  ) {
+    if (err) {
+      appData.error = 1;
+      appData["data"] = "Error Occured!";
+      console.log(err);
+      res.status(400).json(appData);
+    } else {
+      appData["error"] = 0;
+      appData["data"] = rows;
+      res.status(200).json(appData);
+    }
+  });
 });
 
 module.exports = router;
