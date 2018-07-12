4a4dacae925dbb7db2bbb1083fe5933cef405fda
Eric <eric@wsib-innovation.ca>
2018-07-12 17:09:18 -0400

    slew of functions for employer to run check after completing workhistory

--- api/routes/employers.js
+++ api/routes/employers.js
@@ -40,15 +40,142 @@ router.get("/actionClaims/:employerId", (req, res, next) => {
 
 router.patch("/updateWorkHistory", (req, res, next) => {
   let appData = {};
+  const id = req.body.workHistoryId;
+  const claimId = req.body.claimId;
   const data = {
-    id: req.body.workHistoryId,
+    employerConfirmed: 1,
+    employeeId: req.body.employeeId,
     confirmJob: req.body.confirmJob,
     confirmTasks: req.body.confirmTasks,
     comments: req.body.comments || null
   };
+  database.query("UPDATE WorkHistory SET ? WHERE id = ?", [data, id], function(
+    err,
+    rows,
+    fields
+  ) {
+    if (err) {
+      appData.error = 1;
+      appData["data"] = "Error Occured!";
+      res.status(400).json(appData);
+    } else {
+      appData.error = 0;
+      appData["data"] = "Successfully updated Work History for employer!";
+      res.locals.employeeId = data.employeeId;
+      res.locals.claimId = claimId;
+      next();
+    }
+  });
+});
+
+/**
+ * Check if all workHistory has been confirmed for user
+ */
+router.patch("/updateWorkHistory", (req, res, next) => {
+  let appData = {};
+  const id = res.locals.employeeId;
   database.query(
-    "UPDATE WorkHistory SET employerConfirmed = ?, confirmJob = ?, confirmTasks = ?, comments = ? WHERE id = ?",
-    [1, data.confirmJob, data.confirmTasks, data.comments, data.id],
+    "SELECT id FROM WorkHistory WHERE employeeId = ? AND employerConfirmed = ?",
+    [id, 0],
+    function(err, rows, fields) {
+      if (err) {
+        appData.error = 1;
+        appData["data"] = "Error Occured!";
+        res.status(400).json(appData);
+      } else {
+        if (rows.length < 1) {
+          next();
+        } else {
+          appData.error = 0;
+          appData["data"] = "Successfully updated Work History for employer!";
+          res.status(201).json(appData);
+        }
+      }
+    }
+  );
+});
+
+/**
+ * Update employer section of claim to say completed.
+ */
+router.patch("/updateWorkHistory", (req, res, next) => {
+  let appData = {};
+  const actionRequired = "{}";
+  database.query(
+    "UPDATE Claim SET employer = ?, actionRequired = ?",
+    [1, actionRequired],
+    function(err, rows, fields) {
+      if (err) {
+        appData.error = 1;
+        appData["data"] = "Error Occured!";
+        res.status(400).json(appData);
+      } else {
+        next();
+      }
+    }
+  );
+});
+
+/**
+ * Check if every form is completed
+ */
+router.patch("/updateWorkHistory", (req, res, next) => {
+  let appData = {};
+  const claimId = res.locals.claimId;
+  database.query(
+    "SELECT id FROM Claim WHERE id = ? AND employee = ? AND employer = ? AND doctor = ?",
+    [claimId, 1, 1, 1],
+    function(err, rows, fields) {
+      if (err) {
+        appData.error = 1;
+        appData["data"] = "Error Occured!";
+        res.status(400).json(appData);
+      } else {
+        if (rows.length < 1) {
+          appData.error = 0;
+          appData["data"] = "Not all parties are done!";
+          res.status(201).json(appData);
+        } else {
+          next();
+        }
+      }
+    }
+  );
+});
+
+/**
+ * Update Node Array
+ */
+router.patch("/updateWorkHistory", (req, res, next) => {
+  let appData = {};
+  const claimId = res.locals.claimId;
+  const date = new Date();
+  const nodeArray = [
+    {
+      name: "Information Gathering",
+      details:
+        "Information from all parties is currently being gathered, just as you have to fill out your required info, so must your employer and audiologist.",
+      state: 1,
+      nextSteps:
+        "Once all information from all parties has been received the claim will be reveiwed by an adjudicator who will make a decision.",
+      startDate: "00-00-00 00:00:00",
+      endDate: date
+    },
+    {
+      name: "Under Review",
+      details:
+        "All information required to make a decision on your claim has been recieved.",
+      state: null,
+      nextSteps:
+        "You will receive a notification when the adjudicator has made a decision on your claim, this can take up to 3 days.",
+      startDate: date,
+      endDate: "00-00-00 00:00:00"
+    }
+  ];
+
+  database.query(
+    "UPDATE NodeArray SET Step2 = ?, Step3 = ? WHERE claimId = ?",
+    [JSON.stringify(nodeArray[0]), JSON.stringify(nodeArray[1]), claimId],
     function(err, rows, fields) {
       if (err) {
         appData.error = 1;
@@ -56,7 +183,7 @@ router.patch("/updateWorkHistory", (req, res, next) => {
         res.status(400).json(appData);
       } else {
         appData.error = 0;
-        appData["data"] = "Successfully updated Work History for employer!";
+        appData["data"] = "Successfully updated NodeArray !";
         res.status(201).json(appData);
       }
     }
