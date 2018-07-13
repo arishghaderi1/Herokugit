e7c3e8a4161ba8460dd2e3a28de505ee421f595c
Ian Cross <ian@wsib-innovation.ca>
2018-07-13 10:04:55 -0400

    lnsdfkjbasdfg.bjkafgsd

--- api/routes/notifications.js
+++ api/routes/notifications.js
@@ -6,41 +6,39 @@ router.get("/:userId", function(req, res) {
   let appData = {};
   const id = req.params.userId;
   database.query(
-      "SELECT * FROM Notification WHERE userId = ? ORDER BY createdAt DESC",
-      [id],
-      function(err, rows, fields) {
-        if (err) {
-          appData.error = 1;
-          appData["data"] = "Error Occured!";
-          console.log(err);
-          res.status(400).json(appData);
-        } else {
-          appData["error"] = 0;
-          appData["data"] = rows;
-          res.status(200).json(appData);
-        }
+    "SELECT * FROM Notification WHERE userId = ? ORDER BY createdAt DESC",
+    [id],
+    function(err, rows, fields) {
+      if (err) {
+        appData.error = 1;
+        appData["data"] = "Error Occured!";
+        console.log(err);
+        res.status(400).json(appData);
+      } else {
+        res.status(200).json(rows);
       }
-    );
+    }
+  );
 });
 
 router.get("/update/:notifId", function(req, res) {
   let appData = {};
   const id = req.params.notifId;
   database.query(
-      "UPDATE Notification SET isRead = 1 WHERE id = 9",
-      [id],
-      function(err, rows, fields) {
-        if (!err) {
-          appData.error = 0;
-          appData["data"] = "Notification Updated Correctly!";
-          res.status(201).json(appData);
-        } else {
-          appData["data"] = "Error Occured!";
-          appData["error"] = err
-          res.status(400).json(appData);
-        }
+    "UPDATE Notification SET isRead = 1 WHERE id = 9",
+    [id],
+    function(err, rows, fields) {
+      if (!err) {
+        appData.error = 0;
+        appData["data"] = "Notification Updated Correctly!";
+        res.status(201).json(appData);
+      } else {
+        appData["data"] = "Error Occured!";
+        appData["error"] = err;
+        res.status(400).json(appData);
       }
-    );
+    }
+  );
 });
 
 module.exports = router;
